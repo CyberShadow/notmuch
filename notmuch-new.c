b996b77e85778440b2fbe8d999f162e111c109e4
Vladimir Panteleev <vladimir@thecybershadow.net>
2017-08-22 17:54:01 +0000

    Revert "notmuch-new: Fix one unnecessary use of asprintf"
    This reverts commit bad2deeaf53e089d2e66728ec16185c280d62862.

--- notmuch-new.c
+++ notmuch-new.c
@@ -755,7 +755,7 @@ count_files (const char *path, int *count, add_files_state_t *state)
 	    continue;
 	}
 
-	if (talloc_asprintf (&next, "%s/%s", path, entry->d_name) == -1) {
+	if (asprintf (&next, "%s/%s", path, entry->d_name) == -1) {
 	    next = NULL;
 	    fprintf (stderr, "Error descending from %s to %s: Out of memory\n",
 		     path, entry->d_name);
@@ -773,7 +773,7 @@ count_files (const char *path, int *count, add_files_state_t *state)
 	    count_files (next, count, state);
 	}
 
-	talloc_free (next);
+	free (next);
     }
 
   DONE:
